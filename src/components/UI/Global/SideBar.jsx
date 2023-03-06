494598588ec5d112ecc2f6cb435c9d0d7597bd49
zeadhani <zeadhani88@gmail.com>
2023-03-06 18:12:20 +0200

    category home

--- src/components/UI/Global/SideBar.jsx
+++ src/components/UI/Global/SideBar.jsx
@@ -26,71 +26,62 @@ import {
   sidebarClasses,
 } from "react-pro-sidebar";
 import { colors } from "../../../Theme";
-import { motion } from "framer-motion";
 function SideBar() {
-  const { collapseSidebar, collapsed } = useProSidebar();
+  const { collapseSidebar } = useProSidebar();
   const matches = useMediaQuery("(max-width:800px)");
   return (
-    // <Box
-    //   className="sidebar"
-    //   sx={{ display: collapsed ? "none" : "" }}
-    //   component={motion.div}
-    //   initial={{ x: "-100%" }}
-    //   whileInView={{ x: "0%" }}
-    //   transition={{ duration: 0.4 }}
-    // >
-    //   {matches && (
-    //     <Sidebar
-    //       defaultCollapsed={true}
-    //       collapsedWidth={"0px"}
-    //       bor
-    //       rootStyles={{
-    //         [`.${sidebarClasses.container}`]: {
-    //           backgroundColor: "#f8f8f8",
-    //           height: "100vh",
-    //         },
-    //       }}
-    //     >
-    //       <Box bgcolor={colors.grey[900]}>
-    //         <Box display={"flex"} justifyContent={"right"}>
-    //           <IconButton
-    //             onClick={() => collapseSidebar()}
-    //             sx={{ color: "#f5f5f5" }}
-    //           >
-    //             <Close />
-    //           </IconButton>
-    //         </Box>
-    //         <Box
-    //           sx={{
-    //             display: "grid",
-    //             justifyContent: "center",
-    //             gap: 2,
-    //             py: 3,
-    //           }}
-    //         >
-    //           <Avatar alt="Zead" sx={{ width: "120px", height: "120px" }} />
-    //           <Typography textAlign={"center"} color={"#f5f5f5"} variant="h3">
-    //             Zead Hani
-    //           </Typography>
-    //         </Box>
-    //       </Box>
+    <Box className="sidebar">
+      {matches && (
+        <Sidebar
+          defaultCollapsed={true}
+          collapsedWidth={"0px"}
+          bor
+          rootStyles={{
+            [`.${sidebarClasses.container}`]: {
+              backgroundColor: "#f8f8f8",
+              height: "100vh",
+            },
+          }}
+        >
+          <Box bgcolor={colors.grey[900]}>
+            <Box display={"flex"} justifyContent={"right"}>
+              <IconButton
+                onClick={() => collapseSidebar()}
+                sx={{ color: "#f5f5f5" }}
+              >
+                <Close />
+              </IconButton>
+            </Box>
+            <Box
+              sx={{
+                display: "grid",
+                justifyContent: "center",
+                gap: 2,
+                py: 3,
+              }}
+            >
+              <Avatar alt="Zead" sx={{ width: "120px", height: "120px" }} />
+              <Typography textAlign={"center"} color={"#f5f5f5"} variant="h3">
+                Zead Hani
+              </Typography>
+            </Box>
+          </Box>
 
-    //       <Menu>
-    //         <SubMenu icon={<Person />} label="Profile">
-    //           <MenuItem icon={<Dashboard />}>Dashboard</MenuItem>
-    //           <MenuItem icon={<Settings />}>Edit profile</MenuItem>
-    //         </SubMenu>
-    //         <SubMenu icon={<ShoppingCart />} label="Orders">
-    //           <MenuItem icon={<RequestPageSharp />}>Current requests</MenuItem>
-    //           <MenuItem icon={<MoneyOff />}>Past orders</MenuItem>
-    //         </SubMenu>
-    //         <MenuItem icon={<Call />}>Contact us</MenuItem>
-    //         <MenuItem icon={<Help />}>Help</MenuItem>
-    //       </Menu>
-    //     </Sidebar>
-    //   )}
-    // </Box>
-    <></>
+          <Menu>
+            <SubMenu icon={<Person />} label="Profile">
+              <MenuItem icon={<Dashboard />}>Dashboard</MenuItem>
+              <MenuItem icon={<Settings />}>Edit profile</MenuItem>
+            </SubMenu>
+            <SubMenu icon={<ShoppingCart />} label="Orders">
+              <MenuItem icon={<RequestPageSharp />}>Current requests</MenuItem>
+              <MenuItem icon={<MoneyOff />}>Past orders</MenuItem>
+            </SubMenu>
+            <MenuItem icon={<Call />}>Contact us</MenuItem>
+            <MenuItem icon={<Help />}>Help</MenuItem>
+          </Menu>
+        </Sidebar>
+      )}
+    </Box>
   );
 }
 
