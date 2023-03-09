922d2a6d4465da76b92f27e6d1942206f809f3b3
zeadhani <zeadhani88@gmail.com>
2023-03-09 22:48:35 +0200

    background cover

--- src/components/UI/products/productDetails/productDescription/AttributeItem.jsx
+++ src/components/UI/products/productDetails/productDescription/AttributeItem.jsx
@@ -2,11 +2,11 @@ import { Box, Button, Typography } from "@mui/material";
 import React, { useState } from "react";
 import { colors } from "../../../../../Theme";
 
-function AttributeItem({attribute,attributeData}) {
-    const [selectedAttribute, setSelectedAttribute] = useState("");
-    const handleAttributeChange = (data) => () => {
-        setSelectedAttribute(data);
-      };
+function AttributeItem({ attribute, attributeData }) {
+  const [selectedAttribute, setSelectedAttribute] = useState("");
+  const handleAttributeChange = (data) => () => {
+    setSelectedAttribute(data);
+  };
   return (
     <Box my={1}>
       <Typography variant="h5" my={1}>
@@ -23,6 +23,10 @@ function AttributeItem({attribute,attributeData}) {
               sx={{
                 bgcolor: selected ? colors.grey[900] : "",
                 color: selected ? "#f5f5f5" : "",
+                "&:hover": {
+                  bgcolor: selected ? "#f5f5f5" : colors.grey[900],
+                  color: selected ? "#222" : "#f5f5f5",
+                },
               }}
             >
               {item}
