5fa41d6ecc6ee4fc9cec001c532b016069d0e8a1
zeadhani <zeadhani88@gmail.com>
2023-04-09 00:25:06 +0200

    zooming problem

--- src/components/Header/LargeScreen/LargeNavbar.jsx
+++ src/components/Header/LargeScreen/LargeNavbar.jsx
@@ -7,12 +7,24 @@ import { Adb } from "@mui/icons-material";
 import ProfileIcon from "./ProfileIcon";
 
 import SearchBar from "../../Forms/searchBar";
+import { useNavigate } from "react-router-dom";
 function LargeNavbar() {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/");
+  };
   return (
-    <Box sx={{ backgroundImage: `url(${svg})`, py: 1 ,backgroundRepeat:"no-repeat" ,backgroundSize:'cover'}}>
-      <AppBar position="static" sx={{ bgcolor:'transparent' }} elevation={0}>
+    <Box
+      sx={{
+        backgroundImage: `url(${svg})`,
+        py: 1,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <AppBar position="static" sx={{ bgcolor: "transparent" }} elevation={0}>
         <Container maxWidth="lg">
-          <Toolbar disableGutters >
+          <Toolbar disableGutters>
             <Adb sx={{ display: "flex", mr: 2 }} />
             <Typography
               variant="h6"
@@ -24,12 +36,14 @@ function LargeNavbar() {
                 letterSpacing: ".3rem",
                 color: "inherit",
                 textDecoration: "none",
+                cursor: "pointer",
               }}
+              onClick={handleClick}
             >
               BOGO
             </Typography>
             {/* <Links /> */}
-            <SearchBar name={'products'}/>
+            <SearchBar name={"products"} />
             <ProfileIcon />
           </Toolbar>
         </Container>
