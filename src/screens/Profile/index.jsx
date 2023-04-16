33cf6197f4c6a35fbc8d63a761624edd108f7f2f
zeadhani <zeadhani88@gmail.com>
2023-04-16 22:26:51 +0200

    profile info

--- src/screens/Profile/index.jsx
+++ src/screens/Profile/index.jsx
@@ -1,11 +1,18 @@
-import { Avatar, Box, Button, Slide, Typography } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Button,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 import React from "react";
 import CustomContainer from "../../components/UI/Global/CustomContainer";
 import { useSelector } from "react-redux";
 import useUser from "../../hooks/user/useUser";
 import Error from "../../components/UI/Global/Error";
 import LoadingData from "../../components/UI/Global/LoadingData";
-import { ArrowBack, Edit, Lock, Settings } from "@mui/icons-material";
+import { ArrowBack, Edit, Lock, Logout, Settings } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
 
 const ProfilePage = () => {
@@ -13,7 +20,8 @@ const ProfilePage = () => {
   const email = userData.replace(/"/g, "");
   const { data, isLoading, isError } = useUser({ email });
   const navigate = useNavigate();
-
+  console.log(data);
+  const matches = useMediaQuery("(max-width:800px)");
   const handleGoBack = () => {
     navigate(-1);
   };
@@ -25,39 +33,68 @@ const ProfilePage = () => {
   }
   return (
     <CustomContainer nav={"profile"}>
-      <Box my={1}>
+      {matches && (
         <Button
           variant="text"
           color="primary"
           onClick={handleGoBack}
           startIcon={<ArrowBack />}
+          sx={{mt:1}}
         >
           Go Back
         </Button>
-        <Avatar
-          alt={"Profile-Image"}
-          src={`${process.env.REACT_APP_CLOUDINARY}${data?.image}`}
-          sx={{ width: 100, height: 100 }}
-        />
-
-        <Typography variant="h4" textTransform={"capitalize"}>
+      )}
+      <Box my={4}>
+        <Box my={1} mx={"auto"} width={"fit-content"}>
+          <Avatar
+            alt={"Profile-Image"}
+            src={`${process.env.REACT_APP_CLOUDINARY}${data?.image}`}
+            sx={{ width: 200, height: 200 }}
+          />
+        </Box>
+        <Typography
+          textAlign={"center"}
+          variant="h3"
+          textTransform={"uppercase"}
+        >
           {data.first_name + " " + data.last_name}
         </Typography>
-        <Typography variant="subtitle1" color="textSecondary">
-          {data.email}
-        </Typography>
+        <Box
+          display={"flex"}
+          flexDirection={"column"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          gap={1}
+          my={2}
+        >
+          <Typography variant="subtitle1" color="textSecondary">
+            {data.email}
+          </Typography>
+          <Typography variant="subtitle1" color="textSecondary">
+            {data.phone}
+          </Typography>
+          <Typography variant="subtitle1" color="textSecondary">
+            {data.address}
+          </Typography>
+        </Box>
+        <Box
+          display={"flex"}
+          flexWrap={"wrap"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Button startIcon={<Edit />} color="primary">
+            Edit Profile
+          </Button>
 
-        <Button startIcon={<Edit />} color="primary">
-          Edit Profile
-        </Button>
+          <Button startIcon={<Lock />} color="primary">
+            Change Password
+          </Button>
 
-        <Button startIcon={<Settings />} color="primary">
-          Change Password
-        </Button>
-
-        <Button startIcon={<Lock />} color="secondary">
-          Logout
-        </Button>
+          <Button startIcon={<Logout />} color="secondary">
+            Logout
+          </Button>
+        </Box>
       </Box>
     </CustomContainer>
   );
