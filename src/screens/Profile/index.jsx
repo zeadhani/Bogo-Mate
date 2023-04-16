7b65d8c72d37c44472172a0a5c99abcd5c19b5d7
zeadhani <zeadhani88@gmail.com>
2023-04-16 22:31:37 +0200

    profile info

--- src/screens/Profile/index.jsx
+++ src/screens/Profile/index.jsx
@@ -20,7 +20,7 @@ const ProfilePage = () => {
   const email = userData.replace(/"/g, "");
   const { data, isLoading, isError } = useUser({ email });
   const navigate = useNavigate();
-  console.log(data);
+
   const matches = useMediaQuery("(max-width:800px)");
   const handleGoBack = () => {
     navigate(-1);
@@ -39,62 +39,57 @@ const ProfilePage = () => {
           color="primary"
           onClick={handleGoBack}
           startIcon={<ArrowBack />}
-          sx={{mt:1}}
+          sx={{ mt: 1 }}
         >
           Go Back
         </Button>
       )}
-      <Box my={4}>
-        <Box my={1} mx={"auto"} width={"fit-content"}>
-          <Avatar
-            alt={"Profile-Image"}
-            src={`${process.env.REACT_APP_CLOUDINARY}${data?.image}`}
-            sx={{ width: 200, height: 200 }}
-          />
-        </Box>
-        <Typography
-          textAlign={"center"}
-          variant="h3"
-          textTransform={"uppercase"}
-        >
-          {data.first_name + " " + data.last_name}
+
+      <Box my={1} mx={"auto"} width={"fit-content"}>
+        <Avatar
+          alt={"Profile-Image"}
+          src={`${process.env.REACT_APP_CLOUDINARY}${data?.image}`}
+          sx={{ width: 180, height: 180 }}
+        />
+      </Box>
+      <Typography textAlign={"center"} variant="h3" textTransform={"uppercase"}>
+        {data.first_name + " " + data.last_name}
+      </Typography>
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        gap={1}
+        my={2}
+      >
+        <Typography variant="subtitle1" color="textSecondary">
+          {data.email}
         </Typography>
-        <Box
-          display={"flex"}
-          flexDirection={"column"}
-          justifyContent={"center"}
-          alignItems={"center"}
-          gap={1}
-          my={2}
-        >
-          <Typography variant="subtitle1" color="textSecondary">
-            {data.email}
-          </Typography>
-          <Typography variant="subtitle1" color="textSecondary">
-            {data.phone}
-          </Typography>
-          <Typography variant="subtitle1" color="textSecondary">
-            {data.address}
-          </Typography>
-        </Box>
-        <Box
-          display={"flex"}
-          flexWrap={"wrap"}
-          justifyContent={"center"}
-          alignItems={"center"}
-        >
-          <Button startIcon={<Edit />} color="primary">
-            Edit Profile
-          </Button>
+        <Typography variant="subtitle1" color="textSecondary">
+          {data.phone}
+        </Typography>
+        <Typography variant="subtitle1" color="textSecondary">
+          {data.address}
+        </Typography>
+      </Box>
+      <Box
+        display={"flex"}
+        flexWrap={"wrap"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <Button startIcon={<Edit />} color="primary">
+          Edit Profile
+        </Button>
 
-          <Button startIcon={<Lock />} color="primary">
-            Change Password
-          </Button>
+        <Button startIcon={<Lock />} color="primary">
+          Change Password
+        </Button>
 
-          <Button startIcon={<Logout />} color="secondary">
-            Logout
-          </Button>
-        </Box>
+        <Button startIcon={<Logout />} color="secondary">
+          Logout
+        </Button>
       </Box>
     </CustomContainer>
   );
