d8ba68bc2f1f0f38a4760ea537ccbe800214b278
zeadhani <zeadhani88@gmail.com>
2023-06-17 15:35:16 +0300

    homeslider mobile view

--- src/components/UI/home/Mobile/Carousel.jsx
+++ src/components/UI/home/Mobile/Carousel.jsx
@@ -1,18 +1,9 @@
 import React from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
-import Button from '@mui/material/Button'
-import { useEffect, useState } from 'react';
-import Typography from '@mui/material/Typography';
-
+import { Box, Typography, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 function Carousel({ homeSliders }) {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    if (homeSliders) {
-      setIsLoading(false);
-    }
-  }, [homeSliders]);
   const [emblaRef] = useEmblaCarousel(
     {
       axis: "x",
@@ -26,27 +17,21 @@ function Carousel({ homeSliders }) {
       }),
     ]
   );
+  const navigate = useNavigate();
   return (
     <div className="embla">
       <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container">
-          {!isLoading &&
-            homeSliders.map((slide) => (
-              <div className="embla__slide" key={slide.id}>
-                <div
-                  className="embla__slide__content"
-                  style={{
-                    backgroundImage: `linear-gradient(to right bottom, rgba(0, 0, 0, 0.7), rgba(30, 30, 30, 0.8)), url(${
-                      process.env.REACT_APP_CLOUDINARY + slide.image
-                    })`,
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    flexDirection: "column",
-                    color: "#fff",
-                    padding: "1rem",
+          {homeSliders && (
+            <>
+              <div className="embla__slide">
+                <Box
+                  sx={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "30%",
+                    textAlign: "center",
+                    transform: "translate(-50%, -50%)",
                   }}
                 >
                   <Typography
@@ -54,25 +39,106 @@ function Carousel({ homeSliders }) {
                       color: "#f5f5f5f5",
                       textTransform: "uppercase",
                     }}
-                    variant="h1"
+                    variant="h3"
                   >
-                    {slide.title}
+                    {homeSliders[0]?.title}
                   </Typography>
+
+                  <Button
+                    variant="contained"
+                    size="small"
+                    sx={{ mt: "10px", backgroundColor: "#222" }}
+                    onClick={() => navigate("/shop/" + homeSliders[0]?.link)}
+                  >
+                    Shop Now
+                  </Button>
+                </Box>
+                <img
+                  className="embla__slide__img"
+                  src={process.env.REACT_APP_CLOUDINARY + homeSliders[0]?.image}
+                  alt="Your alt text"
+                />
+              </div>
+              <div className="embla__slide">
+                <Box
+                  sx={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    textAlign: "center",
+                    transform: "translate(-50%, -50%)",
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      color: "#f5f5f5f5",
+                      textTransform: "uppercase",
+                    }}
+                    variant="h2"
+                  >
+                    {homeSliders[1]?.title}
+                  </Typography>
+
                   <Button
                     variant="outlined"
+                    size="small"
                     sx={{
                       mt: "10px",
-                      backgroundColor: "transparent",
                       color: "#f5f5f5",
                       border: "1px solid #f5f5f5f5",
                     }}
-                    // onClick={() => navigate("/shop/" + slide.link)}
+                    onClick={() => navigate("/shop/" + homeSliders[1]?.link)}
                   >
-                    DISCOVER
+                    Shop Now
                   </Button>
-                </div>
+                </Box>
+                <img
+                  className="embla__slide__img"
+                  src={process.env.REACT_APP_CLOUDINARY + homeSliders[1]?.image}
+                  alt="Your alt text"
+                />
               </div>
-            ))}
+              <div className="embla__slide">
+                <Box
+                  sx={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    textAlign: "center",
+                    transform: "translate(-50%, -50%)",
+                  }}
+                >
+                  <Typography
+                    sx={{
+                      color: "#f5f5f5f5",
+                      textTransform: "uppercase",
+                    }}
+                    variant="h2"
+                  >
+                    {homeSliders[2]?.title}
+                  </Typography>
+
+                  <Button
+                    variant="outlined"
+                    size="small"
+                    sx={{
+                      mt: "10px",
+                      color: "#f5f5f5",
+                      border: "1px solid #f5f5f5f5",
+                    }}
+                    onClick={() => navigate("/shop/" + homeSliders[2]?.link)}
+                  >
+                    Shop Now
+                  </Button>
+                </Box>
+                <img
+                  className="embla__slide__img"
+                  src={process.env.REACT_APP_CLOUDINARY + homeSliders[2]?.image}
+                  alt="Your alt text"
+                />
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
@@ -80,3 +146,16 @@ function Carousel({ homeSliders }) {
 }
 
 export default Carousel;
+ {/* {Array(3)
+            .fill(0)
+            .map((index) => (
+              <div className="embla__slide" key={Math.random()}>
+                <img
+                  className="embla__slide__img"
+                  src={
+                    "https://res.cloudinary.com/df2862din/image/upload/v1677676881/c1_bz702m.jpg"
+                  }
+                  alt="Your alt text"
+                />
+              </div>
+            ))} */}
