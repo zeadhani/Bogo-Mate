9afd86f97b3ce6702619977f04e00962d3075eb4
zeadhani <zeadhani88@gmail.com>
2023-06-21 20:43:57 +0300

    rollback

--- src/components/UI/products/productDetails/productDescription/AttributeItem.jsx
+++ src/components/UI/products/productDetails/productDescription/AttributeItem.jsx
@@ -3,14 +3,13 @@ import React from "react";
 import { colors } from "../../../../../Theme";
 
 function AttributeItem({ attribute, attributeData, onSelect, selectedValue }) {
-  const attributeSet = new Set([...attributeData]);
   return (
     <Box my={1}>
       <Typography variant="h5" my={1}>
         {attribute}
       </Typography>
       <Box display={"flex"} flexWrap={"wrap"} gap={0.5}>
-        {[...attributeSet]?.map((item, index) => {
+        {attributeData?.map((item, index) => {
           const selected = item === selectedValue;
           return (
             <Button
